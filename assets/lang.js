efc71ac2f4e63de244a59c061f52469501a09391
rendi001-code <bsksahur86@gmail.com>
2026-07-24 15:30:57 +0700

    Delete assets/lang.js

--- assets/lang.js
+++
@@ -1,7 +0,0 @@
-const LANG = {
-  id: { new_chat: "Chat Baru", send: "Kirim" },
-  en: { new_chat: "New Chat", send: "Send" },
-  ms: { new_chat: "Sembang Baru", send: "Hantar" },
-  ar: { new_chat: "دردشة جديدة", send: "إرسال" }
-}
-function setLanguage(lang){ localStorage.setItem('kpaca_ai_lang', lang); document.querySelectorAll('[data-lang]').forEach(el => el.innerText = LANG[lang][el.dataset.lang]); }
