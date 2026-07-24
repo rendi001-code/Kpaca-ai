b9d2f8ca315d88ff3bd7ab2399dacc66e3b382ac
rendi001-code <bsksahur86@gmail.com>
2026-07-24 15:25:50 +0700

    Update config.js

--- assets/config.js
+++ assets/config.js
@@ -1,10 +1,10 @@
 const CONFIG = {
-  app_name: "KPACA AI",
-  version: "2.3",
-  maintenance: false,
-  maintenance_message: "KPACA AI sedang update 10 menit lagi",
-  master_key: "kpaca070112",
-  login_bg_url: "https://freeimage.host/i/COHQ3gf", // GANTI LINK CATBOX KAMU
-  default_models: ["deepseek/deepseek-r1:free", "meta-llama/llama-4-scout:free", "qwen/qwen3-32b:free", "google/gemini-2.5-flash-lite:free"],
-  default_lang: "id"
-  }
+  app_name: "KPACA AI", version: "3.7",
+  supabase_url: "https://tkwseucmgnsggurmkdql.supabase.co",
+  supabase_key: "sb_publishable_9dGrEg8238Jr4FleJnQPnA_Iuw7UNaK",
+  default_api_key: "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",
+  default_models: ["google/gemini-2.0-flash-exp:free", "deepseek/deepseek-r1:free", "meta-llama/llama-4-scout:free"],
+  languages: {"id":"Bahasa Indonesia","en":"English","ms":"Bahasa Melayu","ar":"العربية","zh":"中文","ja":"日本語","ko":"한국어","hi":"हिन्दी","fr":"Français","de":"Deutsch","es":"Español","pt":"Português","ru":"Русский","tr":"Türkçe","vi":"Tiếng Việt","th":"ไทย","it":"Italiano","nl":"Nederlands","pl":"Polski","bn":"বাংলা","auto":"Auto Detect"},
+  default_lang: "id",
+  maintenance_mode: false // ganti true buat maintenance
+}
